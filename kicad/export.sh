7fa185483a376a17d47d8758a843b050c9c9d609
aromero3003 <alromero@fi.uba.ar>
2026-03-08 01:47:07 -0300

    feat(pcb): agrego script para exportar svg

---
+++ kicad/export.sh
@@ -0,0 +1,4 @@
+kicad-cli pcb export svg tp.kicad_pcb \
+--exclude-drawing-sheet \
+--fit-page-to-board \
+-l "Edge.Cuts,F.Silkscreen,B.Silkscreen,B.Fab,F.Fab,B.Paste,F.Paste,F.Courtyard,F.Cu,In2.Cu,In3.Cu,In5.CuIn6.Cu,In7.Cu,In8.Cu,B.Cu"
